929aa5c04fb98c518e46b29d81e9cdfcb1ead122
Shreyas-S-smarty <shreyassrsr@gmail.com>
2024-12-23 15:55:22 +0530

    Add files via upload

---
+++ spotify/frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
